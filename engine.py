b4074af548b66bf5504143c5f99f717fb136816d
b-lovro <lovro.brulec@gmail.com>
2025-09-06 16:13:25 +0200

    cleanup and typing

--- engine.py
+++ engine.py
@@ -1,38 +1,41 @@
 import logging
+from typing import Any, Dict, Optional
 
-import wandb
+import torch
 import torch.nn as nn
-
+from torch.utils.data import DataLoader
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import _LRScheduler
+import wandb
 
 logger = logging.getLogger(__name__)
 
 
 class Metrics:
-    epoch = None
-    train_loss = None
-    val_loss = None
-    train_accuracy = None
-    val_accuracy = None
-    support_loss = None
-    query_loss = None
-    scheduler_lr = None
+    epoch: Optional[int] = None
+    train_loss: Optional[float] = None
+    val_loss: Optional[float] = None
+    train_accuracy: Optional[float] = None
+    val_accuracy: Optional[float] = None
+    support_loss: Optional[float] = None
+    query_loss: Optional[float] = None
+    scheduler_lr: Optional[float] = None
 
 
 class Engine:
-
     def __init__(
         self,
         model: nn.Module,
-        config: dict,
-        hyperparameters: dict,
+        config: Dict[str, Any],
+        hyperparameters: Dict[str, Any],
         experiment_name: str,
         n_epochs: int,
         device: str,
-        training_set, # DataLoader
-        validation_set, # DataLoader
-        test_set, # DataLoader
-        optimizer,
-        scheduler,
+        training_set: DataLoader,
+        validation_set: DataLoader,
+        test_set: DataLoader,
+        optimizer: Optimizer,
+        scheduler: Optional[_LRScheduler],
     ):
         self.model = model
         self.hyperparameters = hyperparameters
@@ -47,7 +50,7 @@ class Engine:
         self.wandb_run = self.wandb_setup(config)
         self.metrics = Metrics()
 
-    def wandb_setup(self, config):
+    def wandb_setup(self, config: Dict[str, Any]) -> wandb.sdk.wandb_run.Run:
         return wandb.init(
             entity="urban-sirca-vrije-universiteit-amsterdam",
             project="EEG-FM",
@@ -55,7 +58,7 @@ class Engine:
             config=config,
         )
 
-    def wandb_log(self):
+    def wandb_log(self) -> None:
         """
         Log metrics to wandb. Values that are not available aren't logged.
         """
@@ -64,7 +67,7 @@ class Engine:
         }
         self.wandb_run.log(present_metrics)
 
-    def console_log(self):
+    def console_log(self) -> None:
         """
         Log metrics to console. Values that are not available aren't logged.
         """
@@ -77,15 +80,15 @@ class Engine:
         )
         logger.info(present_metrics)
 
-    def train(self):
+    def train(self) -> None:
         for epoch in range(self.n_epochs):
             self.train_epoch()
             self.validate_epoch()
             self.wandb_log()
             self.console_log()
 
-    def test(self):
+    def test(self) -> None:
         pass
 
-    def finish(self):
+    def finish(self) -> None:
         self.wandb_run.finish()
--- utils.py
+++ utils.py
@@ -1,31 +1,4 @@
-import os
-import h5py
-import numpy as np
 import torch
-
-
-def get_KU_dataset():
-    dataset_path = "data/KU/KU_dataset.h5"
-    dfile = h5py.File(dataset_path, "r")
-    return dfile
-
-
-def get_folds(n_LOMSO_folds, shuffled_subjects):
-    """
-    Returns a list of lists, where each list contains the subjects for that fold.
-    """
-    folds = []
-    for fold in range(n_LOMSO_folds):
-        folds.append(shuffled_subjects[fold::n_LOMSO_folds])
-    return folds
-
-
-# function that creates training, validation and test sets
-def create_sets(dfile, shuffled_subjects, n_LOMSO_folds):
-    training_set = []
-    validation_set = []
-    test_set = []
-    folds = get_folds(n_LOMSO_folds, shuffled_subjects)
 
 
 def get_optimizer_scheduler(optimizer_name, scheduler_name):
