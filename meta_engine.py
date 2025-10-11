d1fb2be77467c5f6f47d886349d1d6a3b154a0aa
b-lovro <lovro.brulec@gmail.com>
2025-10-11 09:14:11 +0200

    removed no run

--- meta_engine.py
+++ meta_engine.py
@@ -15,8 +15,6 @@ from meta_helpers import (
     sample_support,
     sample_query,
     fetch_by_indices,
-    sample_support_no_run,
-    sample_query_no_run,
 )
 
 
@@ -250,12 +248,6 @@ class MetaEngine(BaseEngine):
         for sid in subjects_batch:
             sup_idx, que_runs = sample_support(sid, self.train_epi, self.K, self.rng)
             que_idx = sample_query(sid, que_runs, self.train_epi, self.Q, self.rng)
-            sup_idx, que_runs = sample_support_no_run(
-                sid, self.train_epi, self.K, self.rng
-            )
-            que_idx = sample_query_no_run(
-                sid, que_runs, self.train_epi, self.Q, self.rng
-            )
             Xs, ys = fetch_by_indices(
                 self.train_ds,
                 self.train_epi,
@@ -353,8 +345,6 @@ class MetaEngine(BaseEngine):
             for _ in range(max(1, E)):
                 sup_idx, que_runs = sample_support(sid, self.val_epi, self.K, rng)
                 que_idx = sample_query(sid, que_runs, self.val_epi, self.Q_eval, rng)
-                # sup_idx, que_runs = sample_support_no_run(sid, self.val_epi, self.K, self.rng)
-                # que_idx = sample_query_no_run(sid, que_runs, self.val_epi, self.Q_eval, self.rng)  
 
                 Xs, ys = fetch_by_indices(
                     self.val_ds,
